61ec2766ae7a462b1874c2676481d03b121b7181
RyannKim327 <rksesgundo123@gmail.com>
2025-11-09 01:49:30 +0800

    11-09-25  1:49 [Mapping]

--- frontend/src/layout/map.tsx
+++ frontend/src/layout/map.tsx
@@ -1,30 +1,31 @@
+import MapComponent from "../component/map";
+
 export default function Map() {
   return (
     <div className="flex flex-col items-center w-full min-h-screen bg-[#f9f9f9] px-6 py-30">
       <div className="text-center mb-8">
-        <h1 className="text-5xl font-bold text-gray-800 mb-2">Monitor Earthquake</h1>
+        <h1 className="text-5xl font-bold text-gray-800 mb-2">
+          Monitor Earthquake
+        </h1>
         <p className="text-gray-600 text-lg">
           Track seismic activity in real time with our interactive map
         </p>
       </div>
 
-      <div className="w-full max-w-3xl bg-white rounded-lg shadow-lg  p-6 mb-6">
-                <div className="rounded-lg overflow-hidden shadow-lg ">
-          <iframe
-            className="w-full aspect-video"
-            frameborder="0"
-            scrolling="no"
-            marginheight="0"
-            marginwidth="0"
-            id="gmap_canvas"
-            src="https://maps.google.com/maps?hl=en&amp;q=Philippines&amp;t=&amp;z=6&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-          ></iframe>
-        </div>
-        <h2 className="text-xl font-semibold text-gray-800 mb-5 mt-5">Recent Earthquake Activity</h2>
-        
+      <div className="w-1/3 bg-white rounded-lg shadow-lg  p-6 mb-6">
+        {/* <div className="rounded-lg overflow-hidden shadow-lg w-full h-full"> */}
+        <MapComponent />
+        {/* </div> */}
+        <h2 className="text-xl font-semibold text-gray-800 mb-5 mt-5">
+          Recent Earthquake Activity
+        </h2>
+
         <div className="space-y-3 mb-6">
           {[1, 2, 3, 4].map((item) => (
-            <div key={item} className="bg-gray-100 rounded-lg p-4 animate-pulse">
+            <div
+              key={item}
+              className="bg-gray-100 rounded-lg p-4 animate-pulse"
+            >
               <div className="h-4 bg-gray-300 rounded w-3/4 mb-2"></div>
               <div className="h-3 bg-gray-300 rounded w-1/2"></div>
             </div>
