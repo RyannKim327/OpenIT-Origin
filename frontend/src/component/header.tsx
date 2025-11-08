be87db6d363bb6a908ece53a2f7017b70f648f5e
RyannKim327 <rksesgundo123@gmail.com>
2025-11-08 14:51:57 +0800

    11-08-25 14:51

--- frontend/src/component/header.tsx
+++ frontend/src/component/header.tsx
@@ -2,13 +2,18 @@ import { Link } from "react-router";
 
 export default function Header() {
   return (
-    <div className="flex flex-row w-full justify-between p-2">
+    <div className="flex flex-row w-full justify-between items-center p-2 shadow">
       <div>
         <span className="font-bold">Quick</span>
         <span className="font-bold text-red-500">Watch</span>
       </div>
-      <nav className="flex flex-row gap-1">
+      <nav className="flex flex-row gap-2 items-center">
         <Link to="/">Home</Link>
+        <Link to="records">Records</Link>
+        <Link to="/map">Map</Link>
+        <Link to="/signup" className="bg-red-500 text-white p-2 rounded-lg">
+          Get Started
+        </Link>
       </nav>
     </div>
   );
--- frontend/src/screen/landing.tsx
+++ frontend/src/screen/landing.tsx
@@ -3,7 +3,7 @@ import Input from "../widget/input";
 export default function Landing() {
   return (
     <div>
-      <h1>Basta landing page tot</h1>
+      <h1>Basta landing page to</h1>
       <Input label="Hello" key="username" type="text" />
     </div>
   );
