093b42626c4b1d9cfd008fae1eb18f57534c3b24
RyannKim327 <rksesgundo123@gmail.com>
2025-11-08 22:53:12 +0800

    11-08-25 22:53 [Chinatown]

--- frontend/src/screen/map.tsx
+++ frontend/src/screen/map.tsx
@@ -1,13 +1,13 @@
-// import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-// import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
-import Map from "ol/Map";
-import View from "ol/View";
-import TileLayer from "ol/layer/Tile";
-import { XYZ } from "ol/source";
+// import Map from "ol/Map";
+// import View from "ol/View";
+// import TileLayer from "ol/layer/Tile";
+// import { XYZ } from "ol/source";
 
 export default function MapView() {
-  const [locator, setLocator] = useState<number[] | null>(null);
+  const [locator, setLocator] = useState<number[]>([]);
 
   const info = {
     x: 12.17,
@@ -15,73 +15,58 @@ export default function MapView() {
     z: 6,
   };
 
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setLocator([position.coords.latitude, position.coords.longitude]);
-        },
-        () => {
-          alert("Sorry po");
-        },
-      );
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(
+  //       (position) => {
+  //         setLocator([position.coords.latitude, position.coords.longitude]);
+  //       },
+  //       () => {
+  //         alert("Sorry po");
+  //       },
+  //     );
+  //   }
+  // }, []);
 
-  new Map({
-    target: "map",
-    layers: [
-      new TileLayer({
-        source: new XYZ({
-          url: "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-        }),
-      }),
-    ],
-    view: new View({
-      center: [info.x, info.y],
-      zoom: info.z,
-    }),
-  });
+  // new Map({
+  //   target: "map",
+  //   layers: [
+  //     new TileLayer({
+  //       source: new XYZ({
+  //         url: "https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  //       }),
+  //     }),
+  //   ],
+  //   view: new View({
+  //     center: [info.x, info.y],
+  //     zoom: info.z,
+  //   }),
+  // });
 
-  //
-  // return (
-  //   <div className="flex flex-col w-full h-full">
-  //     <h1>Map</h1>
-  //     <div className="w-full h-full">
-  //       {/* <MapContainer */}
-  //       {/*   className="w-full h-full" */}
-  //       {/*   center={[info.x, info.y]} */}
-  //       {/*   zoom={info.z} */}
-  //       {/*   scrollWheelZoom={true} */}
-  //       {/* > */}
-  //       {/*   <HeatmapOverlay locations={[[info.x, info.y]]} /> */}
-  //       {/*   <TileLayer */}
-  //       {/*     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' */}
-  //       {/*     // url={`https://tile.openstreetmap.org/${info.z}/${info.x}/${info.y}.png`} */}
-  //       {/*     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" */}
-  //       {/*   /> */}
-  //       {/**/}
-  //       {/*   {locator ? ( */}
-  //       {/*     <Marker position={locator ?? [0, 0]}> */}
-  //       {/*       <Popup> */}
-  //       {/*         A pretty CSS3 popup. <br /> Easily customizable. */}
-  //       {/*       </Popup> */}
-  //       {/*     </Marker> */}
-  //       {/*   ) : null} */}
-  //       {/* </MapContainer> */}
-  //       {/* <Map */}
-  //       {/*   layers={ */}
-  //       {/*     new TileLayer({ */}
-  //       {/*       source: new XYZ({ */}
-  //       {/*         url: `https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'`, */}
-  //       {/*       }), */}
-  //       {/*     }) */}
-  //       {/*   } */}
-  //       {/*   view={ */}
-  //       {/*     new View({ */}
-  //       {/*       center: [info.x, info.y], */}
-  //       {/*       zoom: info.z, */}
-  //       {/*     }) */}
-  //       {/*   } */}
-  //       {/*   }) /> */}
+  return (
+    <div className="flex flex-col w-full h-full">
+      <h1>Map</h1>
+      <div className="w-full h-full">
+        <MapContainer
+          className="w-full h-full"
+          center={[info.x, info.y]}
+          zoom={info.z}
+          scrollWheelZoom={true}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          {/* {locator ? ( */}
+          {/*   <Marker position={locator.length > 0 ? locator : [0, 0]}> */}
+          {/*     <Popup> */}
+          {/*       A pretty CSS3 popup. <br /> Easily customizable. */}
+          {/*     </Popup> */}
+          {/*   </Marker> */}
+          {/* ) : null} */}
+        </MapContainer>
+      </div>
+    </div>
+  );
 }
