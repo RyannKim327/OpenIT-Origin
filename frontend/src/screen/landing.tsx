f57d66a5fb8e97d69070da694c164565c39f2837
RyannKim327 <rksesgundo123@gmail.com>
2025-11-08 18:00:37 +0800

    11-08-25 18:00

--- frontend/src/screen/landing.tsx
+++ frontend/src/screen/landing.tsx
@@ -1,13 +1,22 @@
+import Map from "../layout/map";
+
 export default function Landing() {
   return (
-    <div className="min-h-screen ">
-      <section className="relative bg-gradient-to-br from-red-400 via-[#CE2503] to-orange-100 min-h-screen flex items-center justify-start overflow-x-hidden">
+    <div className="h-full overflow-y-scroll snap-y snap-mandatory">
+      <section className="relative bg-gradient-to-br from-red-400 via-[#CE2503] to-orange-100 min-h-screen flex items-center justify-start overflow-x-hidden snap-start">
         <div className="absolute inset-0 opacity-30">
           <img
             src="src/assets/map.png"
             alt="World Map"
             className="w-full h-full object-cover"
           />
+          {/* <iframe */}
+          {/*   src="https://www.google.com/maps/embed/v1/place?q=Ibabang+Iyam&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8" */}
+          {/*   title="Location" */}
+          {/*   loading="lazy" */}
+          {/*   allowFullScreen={false} */}
+          {/*   className="h-full w-full dark:grayscale dark:invert transition ease-in delay-100" */}
+          {/* /> */}
         </div>
 
         {/* Content */}
@@ -15,12 +24,17 @@ export default function Landing() {
           <div className="max-w-2xl">
             <h1 className="text-5xl lg:text-7xl font-bold text-white leading-tight mb-6">
               <span className="text-black">Stay</span> Ahead of the{" "}
-              <span className="block"><span className="text-black">Shakes</span> with</span>
-              <span className="block text-white/90"><span className="text-black">Quake</span>Watch</span>
+              <span className="block">
+                <span className="text-black">Shakes</span> with
+              </span>
+              <span className="block text-white/90">
+                <span className="text-black">Quake</span>Watch
+              </span>
             </h1>
 
             <p className="text-xl text-white/80 mb-8 leading-relaxed">
-              Real-time earthquake alerts, live tracking, and safety resources all in one place.
+              Real-time earthquake alerts, live tracking, and safety resources
+              all in one place.
             </p>
 
             <div className="flex flex-col sm:flex-row gap-4">
@@ -38,6 +52,8 @@ export default function Landing() {
         <div className="absolute bottom-20 right-20 w-24 h-24 bg-white/5 rounded-full blur-lg"></div>
         <div className="absolute top-1/2 right-0 w-48 h-48 bg-white/5 rounded-full blur-2xl transform translate-x-1/2"></div>
       </section>
+
+      <Map />
     </div>
   );
 }
